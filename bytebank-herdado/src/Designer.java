3ecaf1cd3f66f132d93f2b027c1f15c697f194ac
Rafael Souza <rafael_popo@hotmail.com>
2021-03-12 18:51:12 -0300

    Organizando pastas

--- bytebank-herdado/src/Designer.java
+++
@@ -1,8 +0,0 @@
-//Designer é um Funcionario, Designer herda da class Funcionario
-public class Designer extends Funcionario {
-	
-	public double getBonificacao() {
-		System.out.println("Chamando o método de bonificação do Designer");
-		return 200;
-	}
-}
--- bytebank-herdado/src/EditorVideo.java
+++
@@ -1,8 +0,0 @@
-//EditorVideo é um Funcionario, Designer herda da class EditorVideo
-public class EditorVideo extends Funcionario {
-	
-	public double getBonificacao() {
-		System.out.println("Chamando o método de bonificação do Editor de video");
-		return 150;
-	}
-}
